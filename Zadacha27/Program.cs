002e5c8123ae02328d603f3e50f5d13202d507f8
Gunel <gunelminina@gmail.com>
2022-10-02 23:23:02 +0300

    Zadacha27

---
+++ Zadacha27/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдаёт сумму цифр в числе.
+
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.WriteLine("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+
+int result = 0;
+while (a != 0) 
+{
+    result = result + a % 10;
+    a = a / 10;
+}
+Console.WriteLine(result);
