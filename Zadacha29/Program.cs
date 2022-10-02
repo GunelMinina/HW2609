aa567760c801eb16c48ebd34b09c1de8378e9691
Gunel <gunelminina@gmail.com>
2022-10-02 23:51:00 +0300

    Zadacha29

---
+++ Zadacha29/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.WriteLine("Введите число N: ");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
+for (int i = 0; i < N; i++)
+{
+    array[i]=int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Введен массив:");
+for (int i = 0; i < N; i++)
+{
+    Console.Write($"{array[i]} ");
+}
